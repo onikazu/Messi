3dcc05e1b17691ea3acc574310b65a84b569cd20
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-8.local>
2018-09-11 17:05:00 +0900

    fix

--- src/train.py
+++ src/train.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
 
         # サーバの起動
         cmd = \
-            "rcssserver server::half_time = -1 server::send_step = 3 server::sense_body_step = 2 server::simulator_step = 2 server::auto_mode = true server::kick_off_wait = 200"
+            "rcssserver server::half_time = -1 server::send_step = 3 server::sense_body_step = 2 server::simulator_step = 2 server::auto_mode = true server::kick_off_wait = 400"
         server = subprocess.Popen(cmd.split())
 
         # モニタの起動
